0783bc8b4475cfc0d7febce20a5b3a1e800fe9b2
Nourman Hajar <nourmanhajar@gmail.com>
2023-02-03 00:07:41 +0700

    feat: add react hooks for jikan API

---
+++ src/hooks/useJikan.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import axios, { type AxiosError } from 'axios';
+import useSWRInfinite, { type SWRInfiniteKeyLoader } from 'swr/infinite';
+import type { JikanResponse, Anime } from '../types/jikan';
+
+const fetcher = async (url: string) => {
+  const res = await axios.get<JikanResponse<Anime>>(url);
+  return res.data.data;
+};
+
+const getKey: SWRInfiniteKeyLoader<Anime[][]> = (page, previousPageData) => {
+  if (previousPageData && !previousPageData.length) return null; // reached the end
+  return `/api/v4/top/anime?page=${page + 1}`; // SWR key
+};
+
+function useJikan() {
+  const { data, isLoading, error, size, ...rest } = useSWRInfinite<Anime[], AxiosError>(getKey, fetcher, {
+    initialSize: 1,
+    revalidateFirstPage: false,
+  });
+
+  // Indicates that an additional page (other than the initial page) is being fetched
+  const isLoadingMore = (!data && !error) || (size > 0 && data && typeof data[size - 1] === 'undefined');
+
+  return {
+    data,
+    isLoading,
+    isLoadingMore,
+    error,
+    size,
+    ...rest,
+  };
+}
+
+export default useJikan;
---
+++ src/types/jikan.d.ts
@@ -0,0 +1,130 @@
+export interface JikanResponse<T> {
+  pagination: JikanPagination;
+  data: T[];
+  links: Links;
+}
+
+export interface JikanPagination {
+  last_visible_page: number;
+  has_next_page: boolean;
+  current_page: number;
+  items: {
+    count: number;
+    total: number;
+    per_page: number;
+  };
+}
+
+export interface Links {
+  first: string;
+  last: string;
+  prev: string;
+  next: string;
+}
+
+export interface Anime {
+  mal_id: number;
+  url: string;
+  images: Images;
+  trailer: Trailer;
+  approved: boolean;
+  titles: Title[];
+  title: string;
+  title_english: string;
+  title_japanese: string;
+  title_synonyms: string[];
+  type: string;
+  source: string;
+  episodes: number;
+  status: string;
+  airing: boolean;
+  aired: Aired;
+  duration: string;
+  rating: string;
+  score: number;
+  scored_by: number;
+  rank: number;
+  popularity: number;
+  members: number;
+  favorites: number;
+  synopsis: string;
+  background: string;
+  season: string;
+  year?: number;
+  broadcast: Broadcast;
+  producers: Producer[];
+  licensors: Licensor[];
+  studios: Studio[];
+  genres: Genre[];
+  explicit_genres: Genre[];
+  themes: Theme[];
+  demographics: Demographic[];
+}
+
+export interface AnimeField {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+export type Producer = AnimeField;
+export type Licensor = AnimeField;
+export type Studio = AnimeField;
+export type Genre = AnimeField;
+export type Theme = AnimeField;
+export type Demographic = AnimeField;
+
+export type Images = Record<
+  'jpg' | 'webp',
+  {
+    image_url: string;
+    small_image_url: string;
+    large_image_url: string;
+  }
+>;
+
+export interface TrailerImage {
+  image_url: string;
+  small_image_url: string;
+  medium_image_url: string;
+  large_image_url: string;
+  maximum_image_url: string;
+}
+
+export interface Trailer {
+  youtube_id: string;
+  url: string;
+  embed_url: string;
+  images: TrailerImage;
+}
+
+export interface Title {
+  type: string;
+  title: string;
+}
+
+export interface AiringDate {
+  day: number;
+  month: number;
+  year: number;
+}
+
+export interface AiringDate {
+  from: AiringDate;
+  to?: AiringDate;
+}
+
+export interface Aired {
+  from: Date;
+  to?: Date;
+  prop: AiringDate;
+  string: string;
+}
+
+export interface Broadcast {
+  day: string;
+  time: string;
+  timezone: string;
+  string: string;
+}
