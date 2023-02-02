c1e6c7e245dd7f3c1de5157e091a3cf3d522a81b
Nourman Hajar <nourmanhajar@gmail.com>
2023-02-03 00:14:28 +0700

    feat: revert swrconfig, add api url rewrite

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,24 +5,10 @@ import { SWRConfig, type Cache } from 'swr';
 
 import '../styles/globals.css';
 
-function localStorageProvider() {
-  if (typeof window == 'undefined') return new Map() as Cache;
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const map = new Map(JSON.parse(localStorage?.getItem('app-cache') ?? '[]'));
-  window.addEventListener('beforeunload', () => {
-    const appCache = JSON.stringify(Array.from(map.entries()));
-    localStorage.setItem('app-cache', appCache);
-  });
-  return map as Cache;
-}
-
 const Anicore: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <SWRConfig value={{ provider: localStorageProvider }}>
-        <Component {...pageProps} />
-      </SWRConfig>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 };
