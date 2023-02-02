05980142f8b55912d8ef75b44288658e6b5a8631
Nourman Hajar <nourmanhajar@gmail.com>
2023-02-02 22:01:08 +0700

    chore: set up SWR and Axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,30 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { SWRConfig, type Cache } from 'swr';
 
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+function localStorageProvider() {
+  if (typeof window == 'undefined') return new Map() as Cache;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const map = new Map(JSON.parse(localStorage?.getItem('app-cache') ?? '[]'));
+  window.addEventListener('beforeunload', () => {
+    const appCache = JSON.stringify(Array.from(map.entries()));
+    localStorage.setItem('app-cache', appCache);
+  });
+  return map as Cache;
+}
+
+const Anicore: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <SWRConfig value={{ provider: localStorageProvider }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </SessionProvider>
   );
 };
 
-export default MyApp;
+export default Anicore;
